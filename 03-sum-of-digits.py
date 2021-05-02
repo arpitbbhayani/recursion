36bf57d3a8ba9537dbac1f4cd37f4a048b099393
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-05-03 00:25:18 +0530

    sum of digits.

--- 03-sum-of-digits.py
+++ 03-sum-of-digits.py
@@ -1,9 +1,18 @@
+#
+#  Perform sum of digits using recursion
+#    sum_digits(123) = 1 + 2 + 3 = 6
+#    sod(123) = 3 + sod(12)
+#             = 3 + 2 + sod(1)
+#             = 3 + 2 + 1 + sod(0)
+#             = 6
+#
+
 def _sum(number: int) -> int:
     if number == 0:
         return 0
-    last_digit = number % 10
+    units_digit = number % 10
     remaining_number = number // 10
-    return last_digit + _sum(remaining_number)
+    return units_digit + _sum(remaining_number)
 
 
 def sum_digits(number: int) -> int:
@@ -11,4 +20,4 @@ def sum_digits(number: int) -> int:
 
 
 if __name__ == '__main__':
-    print(sum_digits(14321))
+    print(sum_digits(12321))
