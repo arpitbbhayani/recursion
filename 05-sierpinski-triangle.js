bae3d2e9c9c9ce74536affa4d2e560168e6c481d
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-05-05 01:36:48 +0530

    refactor.

--- 05-sierpinski-triangle.js
+++ 05-sierpinski-triangle.js
@@ -1,21 +1,17 @@
 var canvasWidth = 600;
 var canvasHeight = 600;
-var level = 8;
+var level = 10;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   noLoop();
 }
 
-function getMidpoint(p1, p2) {
-  return createVector((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
-}
-
 function getRandomColor() {
   return color(
-	random(0, 255),
-	random(0, 255),
-	random(0, 255)
+    random(0, 255),
+    random(0, 255),
+    random(0, 255),
   )
 }
 
@@ -24,13 +20,17 @@ function drawTriangle(p1, p2, p3) {
   triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y)
 }
 
+function getMidpoint(p1, p2) {
+  return createVector((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+}
+
 class Triangle {
   constructor(p1, p2, p3) {
-    this.p1 = p1;
-    this.p2 = p2;
-    this.p3 = p3;
+    this.p1 = p1
+    this.p2 = p2
+    this.p3 = p3
   }
-  
+
   draw() {
     drawTriangle(this.p1, this.p2, this.p3)
   }
@@ -44,20 +44,27 @@ function drawSierpinskiTriangle(tri, depth) {
   let m1 = getMidpoint(tri.p1, tri.p2)
   let m2 = getMidpoint(tri.p2, tri.p3)
   let m3 = getMidpoint(tri.p3, tri.p1)
-  let tri_0 = new Triangle(m1, m2, m3)
-  tri_0.draw()
-  
-  drawSierpinskiTriangle(new Triangle(tri.p1, m1, m3), depth + 1)
-  drawSierpinskiTriangle(new Triangle(tri.p2, m2, m1), depth + 1)
-  drawSierpinskiTriangle(new Triangle(tri.p3, m3, m2), depth + 1)
+
+  let t0 = new Triangle(m1, m2, m3)
+  t0.draw()
+
+  let t1 = new Triangle(tri.p1, m1, m3)
+  let t2 = new Triangle(tri.p2, m1, m2)
+  let t3 = new Triangle(tri.p3, m2, m3)
+
+  drawSierpinskiTriangle(t1, depth + 1)
+  drawSierpinskiTriangle(t2, depth + 1)
+  drawSierpinskiTriangle(t3, depth + 1)
 }
 
 function draw() {
   background(250);
-  let p1 = createVector(canvasWidth/2, 0)
+  let p1 = createVector(canvasWidth / 2, 0)
   let p2 = createVector(0, canvasHeight)
   let p3 = createVector(canvasWidth, canvasHeight)
-  let tri_0 = new Triangle(p1, p2, p3)
-  tri_0.draw()
-  drawSierpinskiTriangle(tri_0, 0)
+
+  let t1 = new Triangle(p1, p2, p3)
+  t1.draw()
+
+  drawSierpinskiTriangle(t1, 0)
 }
