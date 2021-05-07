3a3624f7e73c11a4b1bc67e7551291852a3cc34e
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-05-07 12:15:27 +0530

    elementable.

--- 06-word-to-elements-01.py
+++ 06-word-to-elements-01.py
@@ -1,15 +1,39 @@
 import json
 
 with open('./data/elements.json') as fp:
-    elements = json.load(fp)
-    elements_map = {element['symbol'].lower() for element in elements}
+    data = json.load(fp)
+    elements = {element['symbol'].lower() for element in data}
 
 
 def _is_elementable(word: str) -> bool:
-    return False or word
+    # empty word is elementable
+    if not word:
+        return True
+
+    # we compute 3 prefixes from the word
+    # max length of element symbol is 3 hence we are
+    # computing 3 prefixes - of length 1, 2 and 3.
+    prefix1, prefix2, prefix3 = word[:1], word[:2], word[:3]
+
+    # if prefix can be represented as a symbol (elemantable)
+    # remaining word should also be elemntable
+    if prefix1 in elements:
+        if _is_elementable(word[1:]):
+            return True
+
+    if prefix2 in elements:
+        if _is_elementable(word[2:]):
+            return True
+
+    if prefix3 in elements:
+        if _is_elementable(word[3:]):
+            return True
+
+    return False
 
 
 def is_elementable(word: str) -> bool:
+    # lower case the word and check if it is elementable
     return _is_elementable(word.lower())
 
 
@@ -19,6 +43,7 @@ if __name__ == '__main__':
         'newton',
         'ramanujan',
     ]
+
     for in_word in words:
         result = is_elementable(in_word)
         print(f"{in_word} {'is' if result else 'is not'} elementable")
