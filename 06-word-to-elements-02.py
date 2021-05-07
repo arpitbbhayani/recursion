943b9f5d5b46f5ce8485cd8042533199fee29567
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-05-07 13:26:11 +0530

    function rename.

--- 06-word-to-elements-02.py
+++ 06-word-to-elements-02.py
@@ -7,7 +7,7 @@ with open('./data/elements.json') as fp:
     elements = {element['symbol'].lower(): element for element in data}
 
 
-def _get_elements(word: str, symbols: List[str]) -> bool:
+def _get_composition(word: str, symbols: List[str]) -> bool:
     # empty word is elementable
     if not word:
         return True
@@ -21,28 +21,28 @@ def _get_elements(word: str, symbols: List[str]) -> bool:
     # remaining word should also be elemntable
     if prefix1 in elements:
         symbols.append(prefix1)
-        if _get_elements(word[1:], symbols):
+        if _get_composition(word[1:], symbols):
             return True
         symbols.pop()
 
     if prefix2 in elements:
         symbols.append(prefix2)
-        if _get_elements(word[2:], symbols):
+        if _get_composition(word[2:], symbols):
             return True
         symbols.pop()
 
     if prefix3 in elements:
         symbols.append(prefix3)
-        if _get_elements(word[3:], symbols):
+        if _get_composition(word[3:], symbols):
             return True
         symbols.pop()
 
     return False
 
 
-def get_elements(word: str) -> List[str]:
+def get_composition(word: str) -> List[str]:
     symbols = []
-    _get_elements(word.lower(), symbols)
+    _get_composition(word.lower(), symbols)
     return symbols
 
 
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     ]
 
     for in_word in words:
-        decomposed_elements = get_elements(in_word)
+        decomposed_elements = get_composition(in_word)
         if not decomposed_elements:
             print(f"{in_word} is not elementable")
         else:
